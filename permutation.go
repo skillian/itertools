ff1691f3afae64f0671c130ceebc584a16d86c44
Sean Killian <skillian92@gmail.com>
2019-07-16 20:49:40 -0400

    Mainly changes to documentation.
    Plus the wrong slice getting used.

--- permutation.go
+++ permutation.go
@@ -2,16 +2,23 @@ package itertools
 
 // Permuter creates permutations of values.
 type Permuter struct {
-	// A little-endian collection of iterators.
+	// Iterators is the collection of iterators that the Permuter
+	// pulls values from in big-endian order (i.e. The first Iterator in
+	// the slice is the one incremented every call to Next).
 	Iterators []Iterator
-	next      func(p *Permuter, pps []interface{}) bool
+
+	// next holds a function called whenever (*Permuter).Next is called.
+	// The first call to Next needs to initialize the Iterators and so
+	// is initialized with the function, (*Permuter).initialNext and
+	// after that calls (*Permuter).subsequentNext.
+	next func(p *Permuter, pps []interface{}) bool
 }
 
 // NewPermuter creates a new "permuter."
 func NewPermuter(iterators ...Iterator) *Permuter {
 	dups := make([]Iterator, len(iterators))
 	copy(dups, iterators)
-	perm := &Permuter{Iterators: iterators}
+	perm := &Permuter{Iterators: dups}
 	perm.next = (*Permuter).initialNext
 	return perm
 }
@@ -22,7 +29,8 @@ func (p *Permuter) Next(pps ...interface{}) bool {
 	return p.next(p, pps)
 }
 
-// Init both initializes the target pointer values and the underlying iterators.
+// initialNext initializes all of the Permuter's Iterators and the input
+// pointers.
 func (p *Permuter) initialNext(pps []interface{}) bool {
 	for i, it := range p.Iterators {
 		if !it.Next(pps[i]) {
@@ -35,7 +43,8 @@ func (p *Permuter) initialNext(pps []interface{}) bool {
 
 // Next puts the next set of values into the pointers given in pps.  This
 // effectively works as an odometer, rolling over the values in the pointers.
-// the values in the targets of those pointers should start at zero values.
+// the values in the targets of those pointers should start at zero values
+// (as set by (*Permuter).initialNext).
 func (p *Permuter) subsequentNext(pps []interface{}) bool {
 	for i, it := range p.Iterators {
 		pp := pps[i]
